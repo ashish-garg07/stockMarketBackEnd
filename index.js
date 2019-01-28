5f094d2a62375301d9e869a464b4924825464747
Ashish <Ashish@Ashish.Home>
2019-01-28 22:19:20 +0530

    added server send events

--- index.js
+++ index.js
@@ -15,14 +15,58 @@ const port = process.env.PORT || 3002;
 app.get("/", (req, res) => {
   const param = req.query.sym;
   request.get(
-    process.env.API_URL +
-      `&keywords=${param}` +
-      `&apikey=${process.env.API_KEY}`,
+    `${process.env.API_URL}
+      ?function=SYMBOL_SEARCH&keywords=${param}
+      &apikey=${process.env.API_KEY}`,
     (err, response, body) => {
       if (err) {
         return console.dir(err);
       }
       res.send(JSON.parse(body));
+    }
+  );
+});
+
+app.get("/detail", (req, res) => {
+  const param = req.query.sym;
+  request.get(
+    `${
+      process.env.API_URL
+    }?function=TIME_SERIES_INTRADAY&symbol=${param}&interval=1min&apikey=${
+      process.env.API_KEY
+    }`,
+    (err, response, body) => {
+      if (err) {
+        return console.dir(err);
+      }
+      res.send(JSON.parse(body));
+    }
+  );
+});
+
+app.get("/detailWtihSSE", (req, res) => {
+  const param = req.query.sym;
+  console.log(param);
+  request.get(
+    `${
+      process.env.API_URL
+    }?function=TIME_SERIES_INTRADAY&symbol=${param}&interval=1min&apikey=${
+      process.env.API_KEY
+    }`,
+    (err, response, body) => {
+      if (err) {
+        return console.dir(err);
+      }
+
+      res.writeHead(200, {
+        Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache"
+      });
+      setTimeout(() => {
+        res.write("data:" + JSON.stringify(JSON.parse(body)));
+        res.write("\n\n");
+      }, 3000);
     }
   );
 });
